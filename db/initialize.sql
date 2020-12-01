83ce37004c944e5e36e25cdb7f968878f5cac39e
Tayu0404 <tkt.yusei0404@gmail.com>
2020-12-01 12:32:17 +0900

    Update CSV import

--- db/initialize.sql
+++ db/initialize.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS `fss_db`.`users` (
     `user_id`          BIGINT UNSIGNED         NOT NULL,
     `password`         VARCHAR(60)             NOT NULL,
     `locale`           VARCHAR(16),
-    `admin`            BOOLEAN                 NOT NULL DEFAULT 0,
+    `role`             BOOLEAN                 NOT NULL DEFAULT 0,
     `created_at`       TIMESTAMP               NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `last_login`       TIMESTAMP               NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `deleted_at`       TIMESTAMP,
@@ -19,14 +19,14 @@ CREATE TABLE IF NOT EXISTS `fss_db`.`user_profiles` (
     `user_id`          BIGINT UNSIGNED         NOT NULL,
     `name`             VARCHAR(32)             NOT NULL,
     `avatar`           VARCHAR(34)             NOT NULL,
-    `email`            VARCHAR                 NOT NULL,
+    `email`            VARCHAR(128)            NOT NULL,
 
     PRIMARY KEY ( `user_id` )
 );
 
 CREATE TABLE IF NOT EXISTS `fss_db`.`file_types` (
     `filetype_id`      INT UNSIGNED            NOT NULL,
-    `name`             VARCHAR                 NOT NULL,
+    `name`             VARCHAR(128)            NOT NULL,
 
     PRIMARY KEY ( `filetype_id` )
 );
@@ -36,10 +36,10 @@ CREATE TABLE IF NOT EXISTS `fss_db`.`files` (
     `name`             VARCHAR(128)            NOT NULL,
     `user_id`          VARCHAR(32)             NOT NULL,
     `filetype_id`      INT UNSIGNED            NOT NULL,
-    `size`             BIGINT UNSIGNED         NOT NULL,
-    `hash`             CHAR(60)                NOT NULL,
+    `size`             BIGINT UNSIGNED,
+    `hash`             CHAR(60),
     `parent`           VARCHAR(60),
-    `status`           BOOLEAN                 NOT NULL DEFAULT 0,
+    `status`           SMALLINT UNSIGNED       NOT NULL DEFAULT 0,
     `version`          TINYINT(16) UNSIGNED    NOT NULL,
     `created_at`       TIMESTAMP               NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `updated_at`       TIMESTAMP               NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -48,20 +48,7 @@ CREATE TABLE IF NOT EXISTS `fss_db`.`files` (
     PRIMARY KEY ( `file_id` )
 );
 
-CREATE TABLE IF NOT EXISTS `fss_db`.`folders` (
-    `folder_id`        BIGINT UNSIGNED         NOT NULL,
-    `user_id`          VARCHAR(32)             NOT NULL,
-    `name`             VARCHAR(128)            NOT NULL,
-    `parent`           VARCHAR(60),
-    `shared_user`      VARCHAR(32),
-    `created_at`       TIMESTAMP               NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `updated_at`       TIMESTAMP               NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `deleted_at`       TIMESTAMP,
-
-    PRIMARY KEY ( `folder_id` )
-);
-
-CREATE TABLE IF NOT EXISTS `fss_db`.`Shared` (
+CREATE TABLE IF NOT EXISTS `fss_db`.`shared` (
     `shared_id`        BIGINT UNSIGNED         NOT NULL,
     `file_id`          BIGINT UNSIGNED         NOT NULL,
     `permission`       TINYINT(3) UNSIGNED     NOT NULL,
@@ -73,9 +60,16 @@ CREATE TABLE IF NOT EXISTS `fss_db`.`Shared` (
     PRIMARY KEY ( `shared_id` )
 );
 
-BULK INSERT `fss_db`.`Shared`
+BULK INSERT `fss_db`.`shared`
 FROM '/docker-entrypoint-initdb.d/filetype.csv'
 WITH (
     FIRSTROW = 2,
-    FIELDTERMINATOR = ',', 
+    FIELDTERMINATOR = ",",
+    ROWTERMINATOR = "\n"
 );
+
+LOAD DATA INFILE "/docker-entrypoint-initdb.d/filetype.csv"
+    INTO TABLE `fss_db`.`file_types`
+    FIELDS TERMINATED BY ','
+    LINES TERMINATED BY '\n'
+    IGNORE 1 LINES;
